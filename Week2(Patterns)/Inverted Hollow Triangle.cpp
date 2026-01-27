13e44c0cb833971968bc69fddaccb88c47b14125
pratham603-bit <prathamd603@gmail.com>
2026-01-27 23:34:32 +0530

    Done

---
+++ Week2(Patterns)/Inverted Hollow Triangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=n;i>=1;i--){
+    for(int j=1;j<=(n-i);j++){
+        cout<<" ";
+    }
+     for(int j=1;j<=i;j++){
+         if(i==1||i==n||j==1||j==i){
+             cout<<"* ";
+         }else{
+             cout<<"  ";
+         }
+    }
+    cout<<endl;
+    }
+    return 0;
+}
